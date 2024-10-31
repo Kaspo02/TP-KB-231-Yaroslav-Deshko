b6b0d6a7811a2f1ee9de7bd9de550d3bef76ebca
kaspo <2005ajroslav@gmail.com>
2024-10-31 18:35:24 +0200

    add topic_04

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ add solution for task1, task2 and task3
 # 2024-10-11
 add folder topic_3
 add solution for task1, task2, task3 and task4
+
+# 2024-10-31
+add  folder lab_01
+add solution lab_01
+
+add folder topic_4
+add solution topic_04
---
+++ topic_04/task1.py
@@ -0,0 +1,71 @@
+def calculator(opr: str):
+    a, b = enterNum()
+    match opr:
+        case "+":
+            return add(a, b)
+        case "-":
+            return sub(a, b)
+        case "*":
+            return mul(a, b)
+        case "/":
+            return div(a, b)
+        case _:
+            return "Invalid operation"
+        
+        
+def enterNum():
+    try:
+        a = input("a: ")
+        b = input("b: ")
+        return float(a), float(b)
+    except ValueError:
+        print("Err: Please enter valid numbers")
+        raise
+
+def enterOpr():
+    try:
+        opr = input("operation(+, -, *, /) or 'quit' to exit: ").strip().lower()
+
+        valid_operations = ['+', '-', '*', '/', 'quit']
+
+        if opr not in valid_operations:
+            raise ValueError("Invalid operation")
+        
+        return opr
+    
+    except Exception as e:
+        print(f"Error in entering operation: {e}")
+        raise
+    
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Err: Division by zero"
+
+
+
+while True:
+    try:
+        opr = enterOpr()
+        if opr == "quit":
+            print("Exit")
+            break
+            
+        result = calculator(opr)
+        print(result)
+        
+    except KeyboardInterrupt:
+        print("\nProgram interrupted. Exiting...")
+        break
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
